a3b9cdeeb9ce32ffcaf3479c5b2f0500c797a770
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-07-23 16:25:21 +0200

    stringa censurata e nuova lunghezza

--- php-badwords.php
+++ php-badwords.php
@@ -1,5 +1,5 @@
 <!-- Descrizione:
-Creare una variabile con un paragrafo di testo a vostra scelta. Stampare a schermo il paragrafo e la sua lunghezza. Una parola da censurare viene  passata dall'utente tramite parametro GET. Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tuttele  occorrenze della parola da censurare. -->
+Creare una variabile con un paragrafo di testo a vostra scelta. Stampare a schermo il paragrafo e la sua lunghezza. Una parola da censurare viene  passata dall'utente tramite parametro GET. Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le  occorrenze della parola da censurare. -->
 
 
 <?php
@@ -25,8 +25,20 @@ Creare una variabile con un paragrafo di testo a vostra scelta. Stampare a scher
   <?php echo "La lunghezza del paragrafo sopra è di " . strlen($para) . " caratteri."; //calcolo lunghezza paragrafo ?>
 </p>
 <p>
-  <?php echo "La parola da censurare è alla posizione " . strpos($para, $_GET["badword"]); //calcolo lunghezza paragrafo ?>
+  <?php echo "La parola da censurare è alla posizione " . strpos($para, $_GET["badword"]); //calcolo la posizione della parola nell'url ?>
 </p>
+<p>
+  <?php 
+    $newPara = str_replace($_GET["badword"], '***',$para,);
+    echo "La frase censurata di Charlie è: " . $newPara //sostituisco la parola da censurare con ***
+  ?>
+</p>
+<p>
+  <?php 
+    echo "La nuova lunghezza del paragrafo censurato è di " . strlen($newPara) . " caratteri."; //calcolo lunghezza paragrafo 
+  ?>
+</p>
+
 
   
 </body>
